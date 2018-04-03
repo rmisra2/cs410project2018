0d6849e2cc4f4155ea2d8c55a747f8423a3efe43
Varun Munjeti <vrunjeti@gmail.com>
2018-04-03 16:49:24 -0500

    barebones dataset processing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cs410project2018
-CS 410 projet 2018
+
+## Setup
+Use python3
+
+Create a new folder called `data`. This will contain our program's data files, whereas `dataset` will contain what is provided from Yelp.
+
+## Processing Dataset
+In `main.py`, uncomment the call to `parse_reviews_dataset()` and run it. This will create the reviews data we will need. It's slow and uses a lot of RAM, so make sure you comment it out after you run it once.
---
+++ main.py
@@ -0,0 +1,47 @@
+import json
+
+infile = 'dataset/review.json'
+
+# TODO: make sure data folder exists
+outfile = 'data/reviews.json'
+
+def parse_reviews_dataset():
+    """
+    creates a json file of all yelp dataset reviews with necessary attributes
+    """
+    reviews = []
+
+    with open(infile) as f:
+        for line in f:
+            review_data = json.loads(line)
+            review = {
+                'review_id': review_data.get('review_id'),
+                'user_id': review_data.get('user_id'),
+                'business_id': review_data.get('business_id'),
+                'text': review_data.get('text')
+            }
+            reviews.append(review)
+
+    with open(outfile, 'w') as outf:
+        json.dump(reviews, outf)
+
+def group_reviews_by_users(reviews):
+    """
+    given a list of reviews, this will group them by user
+    returns a dict where the key is the user_id and the value is a list of their reviews
+    """
+    user_reviews = {}
+    for review in reviews:
+        user_id = review.get('user_id')
+        if user_id not in user_reviews:
+            user_reviews[user_id] = []
+        review_without_user_id = {
+            'review_id': review.get('review_id'),
+            'business_id': review.get('business_id'),
+            'text': review.get('text')
+        }
+        user_reviews[user_id].append(review_without_user_id)
+    return user_reviews
+
+
+# parse_reviews_dataset()
