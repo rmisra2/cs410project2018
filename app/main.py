42f0453f5bfe9a5166e2b6be5ff54b7498ca33d7
Varun Munjeti <vrunjeti@gmail.com>
2018-04-27 02:44:27 -0500

    calculate average similarity of user reviews

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 import networkx as nx
 import os, json
 from similarity_graph import create_similarity_graph
-from process_data import create_combined_user_reviews
+from process_data import group_reviews_by_users
 from clustering import convert_similarity_graph_to_nx_graph
 
 dirname = os.path.dirname(__file__)
-reviews_file = os.path.join(dirname, './../data/reviews_100.json')
+reviews_file = os.path.join(dirname, './../data/reviews_1000.json')
 
 with open(reviews_file) as rf:
     reviews = json.load(rf)
-    combined_user_reviews = create_combined_user_reviews(reviews)
-    similarity_graph = create_similarity_graph(combined_user_reviews)
+    grouped_user_reviews = group_reviews_by_users(reviews)
+    similarity_graph = create_similarity_graph(grouped_user_reviews)
     graph = convert_similarity_graph_to_nx_graph(similarity_graph, 0.1)
     print(nx.clustering(graph))
