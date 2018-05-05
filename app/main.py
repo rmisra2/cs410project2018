f351f6291c9c9f6ecf169808410b4bf444f2c23d
Varun Munjeti <vrunjeti@gmail.com>
2018-05-05 02:18:16 -0500

    output partition to file

--- app/main.py
+++ app/main.py
@@ -4,14 +4,24 @@ import community
 from similarity_graph import create_similarity_graph
 from process_data import group_reviews_by_users
 from clustering import convert_similarity_graph_to_nx_graph
+from reverse_index import create_adjusted_reviews_for_restaurants
+
+NUM_REVIEWS = 200
 
 dirname = os.path.dirname(__file__)
-reviews_file = os.path.join(dirname, './../data/reviews_200.json')
+reviews_file = './../data/reviews_{}.json'.format(NUM_REVIEWS)
+reviews_filename = os.path.join(dirname, reviews_file)
 
-with open(reviews_file) as rf:
-    reviews = json.load(rf)
-    grouped_user_reviews = group_reviews_by_users(reviews)
-    similarity_graph = create_similarity_graph(grouped_user_reviews)
-    graph = convert_similarity_graph_to_nx_graph(similarity_graph, 0.1)
-    partition = community.best_partition(graph)
-    print(partition)
+def create_partition():
+    with open(reviews_filename) as rf:
+        reviews = json.load(rf)
+        grouped_user_reviews = group_reviews_by_users(reviews)
+        similarity_graph = create_similarity_graph(grouped_user_reviews)
+        graph = convert_similarity_graph_to_nx_graph(similarity_graph, 0.1)
+        partition = community.best_partition(graph)
+
+        partition_file = './../data/partition_{}'.format(NUM_REVIEWS)
+        partition_filename = os.path.join(dirname, partition_file)
+        with open(partition_filename, 'w') as pf:
+            json.dump(partition, pf)
+
